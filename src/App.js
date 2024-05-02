866b74702927ae6f0ab49e8bd82d43f0ba8dfc01
didiprayoyo <adipra1337@gmail.com>
2024-05-02 15:33:10 +0700

    refactor: finish the app for now to be tested

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom'; // render to specific DOM
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 // di react auto udah ada jsx, cek aja modules
@@ -7,13 +6,15 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import About from './pages/About';
+import { RandomComments } from './pages/RandomComments'; // export tanpa default, bisa juga dari sini, tapi ini pages, best practicenya import 1 default pages utama aja
+import ImageSearch from './pages/ImageSearch';
+import VideoSearch from './pages/VideoSearch';
+import Playground from './pages/Playground';
 
-// how to do best practices for components and using them
-import Navbar from './components/Navbar';
-import RandomComments from './components/RandomComments';
+import { Navbar, BootstrapNavbar } from './navbar/Navbar.js';
 
+// Beda jsx dan component, serta cara pakainya
 const Desc = <h1>This is React</h1>;
-
 const DescCard = () => {
     return (
         <div class="card">
@@ -26,16 +27,23 @@ const DescCard = () => {
 
 const App = () => {
     return (
-        <Router>
-            <Navbar />
-            <DescCard />
-            <Routes>
-                <Route path="/" exact element={<Home />} />
-                <Route path="/contact" element={<Contact contacts />} />
-                <Route path="/comment" element={<RandomComments />} />
-                <Route path="/about" element={<About />} />
-            </Routes>
-        </Router>
+        <>  
+            <Router>
+                {/* navbar harus dalem BrowserRouter biar bisa didestructure */}
+                <BootstrapNavbar />
+                <Routes>
+                    <Route path="/" exact element={<Home />} />
+                    <Route path="/contact" element={<Contact contacts />} />
+                    <Route path="/comment" element={<RandomComments />} />
+                    <Route path="/image-search" element={<ImageSearch />} />
+                    <Route path="/video-search" element={<VideoSearch />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/playground" element={<Playground />} />
+                </Routes>
+            </Router>
+
+            {/* <DescCard /> */}
+        </>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // render to specific DOM
 import App from './App'; // This line imports the main App component
 
-ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById('root'));
+// ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById('root')); // deprecated
+const el = document.getElementById('root'); // get reference to div with id=root
+const root = ReactDOM.createRoot(el); // tell react to take control that element
+root.render(<React.StrictMode><App /></React.StrictMode>);
